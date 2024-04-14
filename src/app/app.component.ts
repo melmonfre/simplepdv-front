a28e2ec40fa665b07a8d9d266ee32cd4c067ee56
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2024-04-14 17:02:16 -0300

    Importando componente de menu e exibindo ele na tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from '../shared/menu/menu.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
