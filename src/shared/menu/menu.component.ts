d461309130a1a2f0b7084ac8df86c98d461dd8dd
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2024-04-14 17:03:22 -0300

    Estruturando componente de menu e adcionando arquivo correto de estilo com a extensão apropriada

--- src/shared/menu/menu.component.ts
+++ src/shared/menu/menu.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
+  standalone: true,
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
 
