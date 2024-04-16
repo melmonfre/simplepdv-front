3075c6cd6bba772e932b35abf35bdce3b06a17be
Hynaell Alves <hynaell.alves@ba.estudante.senai.br>
2024-04-16 09:46:34 -0300

    Refazendo componente de Menu usando Angular Material

--- src/shared/menu/menu.component.ts
+++ src/shared/menu/menu.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { DashboardComponent } from '../../app/pages/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
+  imports: [MatSidenavModule, DashboardComponent],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class MenuComponent {
 
 }
