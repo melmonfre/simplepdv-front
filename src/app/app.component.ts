bf1dfb34301d056ded3c8fa02f5391d6c4f0dfa3
Diana Regina <devdianaregina@gmail.com>
2024-04-18 22:18:13 -0300

    :tada: tela de cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'diana';
-}
+export class AppComponent {}
---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [],
+  exportAs: '[RegisterComponent]',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {}
